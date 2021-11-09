4fb4b908a08065fc64798d59b80da1c8ff3affac
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-11-09 14:26:10 +0100

    remove cache

--- cache/production/data_cfg_wefrag.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1654156666
-163
-a:5:{s:4:"name";s:6:"wefrag";s:9:"copyright";s:15:"© Wefrag, 2007";s:13:"style_version";s:5:"3.2.8";s:13:"phpbb_version";s:5:"3.2.8";s:8:"filetime";i:1622619105;}
